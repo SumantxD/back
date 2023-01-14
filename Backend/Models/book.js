8d2591ecee7e173c6e48a72f200b351bfe476c98
Sumant.xD <st72365@gmail.com>
2023-01-14 20:33:09 +0530

    made few chages

--- Backend/Models/book.js
+++ Backend/Models/book.js
@@ -1,20 +1,13 @@
 const mongoose = require('mongoose');
 
-const Schema=mongoose.Schema;
-const bookSchema=new Schema({
-    title:{
-        type:String,
-        required:true,
-    },
-    author:{
-        type:String,
-        required:true,
-    },
-    description:{
-        type:String,
-        required:true,
-    }
+const bookSchema = new mongoose.Schema({
+    title: {type:String, required:true},
+    author: {type:String, required:true},
+    description: {type:String, required:true},
+},{
+    timestamps: true,
+});
 
-},{timestamps:true})
-const book=mongoose.model('Book',bookSchema)
-module.exports = book
+const Book = mongoose.model('book', bookSchema)
+
+module.exports = Book
--- Backend/server.js
+++ Backend/server.js
@@ -1,37 +1,48 @@
 const express = require('express');
  const mongoose = require('mongoose');
-const book = require('./Models/book');
- 
-// const dotenv = require('dotenv');
+
+const Book = require('./models/book');
+
 const app=express();
+
 mongoose.set('strictQuery', false);
- const hello="mongodb+srv://ishikaback:<password>@cluster0.6wobkbs.mongodb.net/Library?retryWrites=true&w=majority"
-//const app=express();
+
+
+const hello="mongodb+srv://ishikaback:<password>@cluster0.6wobkbs.mongodb.net/Library?retryWrites=true&w=majority"
+
 mongoose.connect(hello)
-    .then(()=>{
-        app.listen(3000, ()=>{console.log("server connected to database")});
+    .then((result)=>{
+        console.log('connected to the database')
+        app.listen(3000);
     })
-    .catch(err=>{console.log(err)})
+    .catch((err)=>{console.log(err)})
+
+
 app.get ('/home', (req, res) => {
     res.json({
-        name:"ishia"})
-})
-app.post ('/books', (req, res) => {
-    const book2=new book(req.body)
-    book2.save()
-    .then((resu)=>{
-        
-
+        name:"ishia"
     })
-    .catch(err=>{console.log(err)})
+})
+
+
+app.post ('/books', (req, res) => {
+
+    const book = new Book(req.body)
+
+    book.save()
+        .then((result)=>{
+            res.redirect('/home')
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
 
     
 })
-app.put ('/books/:id', (req, res) => {
-    res.json({name:"is"})
+
+app.use((req,res) => {
+    res.json({
+        log: "404 not found"
+    })
 })
-app.delete ('/books/:id', (req, res) => {
-    res.json({name:"ishi"})
-})
-//app.listen(3000, ()=>{console.log("server started")});
 
